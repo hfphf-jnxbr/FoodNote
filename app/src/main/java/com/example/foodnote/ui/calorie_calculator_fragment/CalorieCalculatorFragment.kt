4c60cbba5d9ae1af6c19e60a1b071d7c6c288e15
dima <testImplementation>
2022-07-04 02:59:23 +0500

    add card view into fragment calorie calculator

--- app/src/main/java/com/example/foodnote/ui/calorie_calculator_fragment/CalorieCalculatorFragment.kt
+++ app/src/main/java/com/example/foodnote/ui/calorie_calculator_fragment/CalorieCalculatorFragment.kt
@@ -1,12 +1,24 @@
 package com.example.foodnote.ui.calorie_calculator_fragment
 
 
+import android.os.Bundle
+import android.view.View
 import com.example.foodnote.databinding.FragmentCalorieCalculatorBinding
 import com.example.foodnote.ui.base.BaseViewBindingFragment
+import java.time.LocalDateTime
 
 
 class CalorieCalculatorFragment :
     BaseViewBindingFragment<FragmentCalorieCalculatorBinding>(FragmentCalorieCalculatorBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
 
-  
+    private fun initView() = with(binding) {
+        val date = LocalDateTime.now()
+        dayWeekMaterialTextView.text = date.toString()
+        circleDiagramView.start(1222, 1500, 100, 150, 30, 70)
+    }
+
 }
