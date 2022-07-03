bab4e4e0492a114b07cd99eacb9afd60ebc59082
berni <bernikov_1993@mail.ru>
2022-07-03 22:05:21 +0500

    fix navigation

--- app/src/main/java/com/example/foodnote/MainActivity.kt
+++ app/src/main/java/com/example/foodnote/MainActivity.kt
@@ -1,58 +1,19 @@
 package com.example.foodnote
 
 import android.os.Bundle
-import android.view.WindowManager
-import androidx.activity.viewModels
-import androidx.annotation.MenuRes
-import androidx.annotation.NavigationRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.foodnote.databinding.ActivityMainBinding
 import com.example.foodnote.ui.base.interfaces.OnBackPressedListener
-import com.example.foodnote.ui.base.viewModel.MainViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private val viewModel: MainViewModel by viewModels()
-    private var navHostFragment: NavHostFragment? = null
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var destinationChangeListener: NavController.OnDestinationChangedListener
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.container_root) as NavHostFragment
-        this.navHostFragment = navHostFragment
-        val navController = navHostFragment.navController
-        @MenuRes val menu: Int = R.menu.bottom_navigation_menu
-        @NavigationRes val navigationGraph: Int = R.navigation.navigation_root
-        destinationChangeListener = NavController.OnDestinationChangedListener { _, _, _ ->
-            viewModel.selectedItemId = binding.bottomNavigation.selectedItemId
-        }
-
-
-        navController.graph = navController.navInflater.inflate(navigationGraph)
-        binding.bottomNavigation.menu.clear()
-        binding.bottomNavigation.inflateMenu(menu)
-        binding.bottomNavigation.setupWithNavController(navController)
-        binding.bottomNavigation.setOnItemReselectedListener {}
-        binding.bottomNavigation.setOnItemSelectedListener { item ->
-            NavigationUI.onNavDestinationSelected(item, navController)
-            return@setOnItemSelectedListener true
-        }
-        val w = window
-        w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        w.statusBarColor = ContextCompat.getColor(this, R.color.black)
-        viewModel.selectedItemId = bundle?.getInt(KEY_SELECTED_ITEM)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val navController = findNavController(R.id.container)
+        bottomNavigationView.setupWithNavController(navController)
     }
-
-    companion object {
-        private const val KEY_SELECTED_ITEM = "key_selected_item"
-    }
-
 
     override fun onBackPressed() {
         // Получаем фрагмент, который в данный момент видит пользователь (согласно графу navigation_root.xml)
--- app/src/main/java/com/example/foodnote/ui/base/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/foodnote/ui/base/viewModel/MainViewModel.kt
@@ -3,5 +3,5 @@ package com.example.foodnote.ui.base.viewModel
 import com.example.foodnote.data.base.AppState
 
 class MainViewModel : BaseViewModel<AppState<*>>() {
-    var selectedItemId: Int? = null
+
 }
