63aaf40876eab6a3a5337e99e5eb13fae3f15bea
dima <testImplementation>
2022-07-04 17:41:47 +0500

    add icon menu

--- app/src/main/java/com/example/foodnote/ui/calorie_calculator_fragment/CalorieCalculatorFragment.kt
+++ app/src/main/java/com/example/foodnote/ui/calorie_calculator_fragment/CalorieCalculatorFragment.kt
@@ -5,24 +5,35 @@ import android.os.Bundle
 import android.view.View
 import com.example.foodnote.databinding.FragmentCalorieCalculatorBinding
 import com.example.foodnote.ui.base.BaseViewBindingFragment
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
 
 
 class CalorieCalculatorFragment :
     BaseViewBindingFragment<FragmentCalorieCalculatorBinding>(FragmentCalorieCalculatorBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initDate()
     }
 
     override fun onResume() {
         super.onResume()
-        initView()
+        initCircle()
     }
 
-    private fun initView() = with(binding) {
-        val date = LocalDateTime.now()
-        dayWeekMaterialTextView.text = date.toString()
+    private fun initCircle() = with(binding) {
         circleDiagramView.start(1222, 1500, 100, 150, 30, 70)
+    }
+
+    private fun initDate() = with(binding) {
+        val current = LocalDateTime.now()
+        val date: String = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+            .withLocale(Locale("ru"))
+            .format(LocalDate.of(current.year, current.month, current.dayOfMonth))
+        dayWeekMaterialTextView.text = date
     }
 
 }
