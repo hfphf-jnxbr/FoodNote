aea28074ad4da4111d193842408d384e61a0bc81
berni <bernikov_1993@mail.ru>
2022-07-10 23:58:18 +0500

    add fragment recipes

--- app/src/main/java/com/example/foodnote/ui/recipes_fragment/RecipesAdapter.kt
+++ app/src/main/java/com/example/foodnote/ui/recipes_fragment/RecipesAdapter.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.RequestManager
 import com.example.foodnote.databinding.ItemRecipesBinding
 
-class RecipesAdapter : ListAdapter<Recipes, RecipesViewHolder>(DIFF_CALLBACK) {
+class RecipesAdapter(private var glide: RequestManager,private val listener: RecipesListener) : ListAdapter<Recipes, RecipesViewHolder>(DIFF_CALLBACK) {
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Recipes>() {
             override fun areItemsTheSame(oldItem: Recipes, newItem: Recipes) =
@@ -18,11 +19,16 @@ class RecipesAdapter : ListAdapter<Recipes, RecipesViewHolder>(DIFF_CALLBACK) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
-        RecipesViewHolder(
-            ItemRecipesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        RecipesViewHolder(listener,
+            ItemRecipesBinding.inflate(LayoutInflater.from(parent.context), parent, false),glide
         )
 
     override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
+}
+
+interface RecipesListener{
+    fun onClickRecipes(recipes: Recipes)
+    fun onClickRecipesLike(recipes: Recipes)
 }
--- app/src/main/java/com/example/foodnote/ui/recipes_fragment/RecipesViewHolder.kt
+++ app/src/main/java/com/example/foodnote/ui/recipes_fragment/RecipesViewHolder.kt
@@ -1,12 +1,19 @@
 package com.example.foodnote.ui.recipes_fragment
 
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import com.example.foodnote.databinding.ItemRecipesBinding
 
 class RecipesViewHolder(
+    private val listener: RecipesListener,
     private val binding: ItemRecipesBinding,
+    private var glide: RequestManager
 ): RecyclerView.ViewHolder(binding.root){
     fun bind(item: Recipes) {
         binding.textViewNameRecipes.text = item.nameRecipes
+        glide.load(item.imageUrl).into(binding.imageViewRecipes)
+        binding.cardViewRecipes.setOnClickListener { listener.onClickRecipes(item) }
+        binding.imageViewRecipesLike.setOnClickListener { listener.onClickRecipesLike(item) }
     }
 }
