210fa29292160acceb4cb2e85e93325f45914fbd
dima <testImplementation>
2022-07-11 02:34:04 +0500

    add build config interceptor

---
+++ app/src/main/java/com/example/foodnote/ui/diary_item_deatil_fragment/DiaryItemDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.example.foodnote.ui.diary_item_deatil_fragment
+
+import com.example.foodnote.databinding.FragmentDiaryItemDetailBinding
+import com.example.foodnote.ui.base.BaseViewBindingFragment
+
+
+class DiaryItemDetailFragment :
+    BaseViewBindingFragment<FragmentDiaryItemDetailBinding>(FragmentDiaryItemDetailBinding::inflate) {
+
+
+}
