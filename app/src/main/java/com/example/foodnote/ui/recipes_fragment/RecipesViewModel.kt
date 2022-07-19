e18d62ecd03e7f0ed08b29622fd541e079a8ce06
berni <bernikov_1993@mail.ru>
2022-07-19 19:54:02 +0500

    add fragment recipes

--- app/src/main/java/com/example/foodnote/ui/recipes_fragment/RecipesViewModel.kt
+++ app/src/main/java/com/example/foodnote/ui/recipes_fragment/RecipesViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.foodnote.ui.recipes_fragment
 
+import androidx.lifecycle.ViewModel
 import com.example.foodnote.data.base.AppState
+import com.example.foodnote.data.base.SampleState
+import com.example.foodnote.data.repository.datastore_pref_repository.UserPreferencesRepository
 import com.example.foodnote.ui.base.viewModel.BaseViewModel
 
-class RecipesViewModel : BaseViewModel<AppState<*>>(){
+class RecipesViewModel( private val dataStorePref: UserPreferencesRepository) : BaseViewModel<SampleState>(dataStorePref){
 }
